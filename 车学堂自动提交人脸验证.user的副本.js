8dc6cc72f9c6cb4166062b57b33ae6a5d2cd7c11
ECH2O <zengmeng@hrbeu.edu.cn>
2019-07-19 00:00:29 +0800

    Update 车学堂自动提交人脸验证.user的副本.js

--- 车学堂自动提交人脸验证.user的副本.js
+++ 车学堂自动提交人脸验证.user的副本.js
@@ -1,52 +1,3 @@
-// ==UserScript==
-// @name          车学堂自动提交人脸验证
-// @namespace    kk
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://learn.drivedu.com.cn/course/index/*
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-    // Your code here...
-
-    console.log('车学堂 hack 加载成功...')
-    var t ="start";
-    var e =1;
-    var str = location.href;
-    var index = str .lastIndexOf("\/");
-    str  = str .substring(index + 1, str .length);
-
-    var a = str.split('.')[0];
-    localStorage.setItem('s', 0); // 今日热销
-
-    if(localStorage.getItem('driveduData')===null){
-        var  result = window.prompt('请输入你的人脸认证照片 base64位编码', '');
-
-        if(result.indexOf('base64')!==-1){
-            localStorage.setItem('driveduData',result)
-        }else{
-            alert('数据输入错误，脚本加载失败！')
-        }
-
-    }
-    var s =  result
-
-    setInterval(function () {
-        if (!$('.promopt1-box').is(":hidden")) {
-
-            collect_take_snapshot(t,e,a)
-        }
-    }, 2000);
-
-    function collect_take_snapshot(t, e, a) {
-        // shutter.play(), Webcam.snap(function (s) {
-        //
-        // })
-        $.ajax({// ==UserScript==
 // @name          车学堂自动提交人脸验证
 // @namespace    kk
 // @version      0.1
@@ -112,6 +63,51 @@
         })
     }
 })();
+          
+            
+            
+            
+            },
+// ==UserScript==
+// @name          车学堂自动提交人脸验证
+// @namespace    kk
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://learn.drivedu.com.cn/course/index/*
+// @grant        none
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+
+    $('body').removeAttr("onblur");
+
+    console.log('车学堂 hack 加载成功...')
+    first();
+    hook();
+    var onAuth = true
+    var t ="start";
+    var e =1;
+    var str = location.href;
+    var index = str .lastIndexOf("\/");
+    str  = str .substring(index + 1, str .length);
+    var a = str.split('.')[0];
+    // 人脸识别图片数据 base64 编码
+    var s = "imgData"
+
+
+    function first(){
+        scaleImg('请手动配置你自己的 base64 编码的头像，并删除此处代码')
+    }
+
+    function collect_take_snapshot(t, e, a) {
+
+        $.ajax({
             url: "/verify/authVerify",
             type: "POST",
             dataType: "json",
@@ -124,15 +120,25 @@
             },
             success: function (e) {
                 if(e.code ===0){
-                    $(".promopt1-box").hide()
+                    scaleImg('已为您自动提交了人脸认证')
+                    setTimeout(function(){$('.close').click()},2500)
+
+                }else{
+                    scaleImg(e.msg)
+
                 }
-                1 == parseInt(e.code) ? (parseInt(e.result) < 3 ? $(".hint").html("<p>您还有<span>" +
-                                                                                  (3 - parseInt(e.result)) + "</span>次机会</p>") : $(".hint").html(
-                    "<p>很遗憾，您本次没有验证机会了，10分钟过后再试！</p>"), $("#notice").attr("class",
-                                                                          "icon-error-coarse"), $("#notice").html("<p>验证失败</p>")) : "start" == t ||
-                    "stop" == t || "playing" == t ? ($(".collect-photo-bg").remove(), void 0 !=
-                                                     player && player.start()) : location.href = "/member/index"
+
             }
         })
     }
+
+
+    function hook() {
+        var f = onStart;         // 保存旧函数
+        onStart = function() {   // 定义新函数
+            collect_take_snapshot(t, e, a)
+        }
+    }
+
+
 })();
